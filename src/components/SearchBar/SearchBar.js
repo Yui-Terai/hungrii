a9dd750f7d5fb079190c29cb94b288f417a47379
Yui Terai <yuiterai88@gmail.com>
2019-08-30 17:46:46 +0800

    Set up App component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,18 +16,20 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    <div className="SearchBar">
-      <div className="SearchBar-sort-options">
-        <ul>{this.renderSortByOptions(sortByOptions)}</ul>
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions(sortByOptions)}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
       </div>
-      <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
-      </div>
-      <div className="SearchBar-submit">
-        <a>Let's Go</a>
-      </div>
-    </div>;
+    );
   }
 }
 
