2fa1d6750b1ae6597ea464bf1f22e58b7b97828b
Yui Terai <yuiterai88@gmail.com>
2019-09-01 21:10:24 +0800

    Add a function to simulate a search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,23 +16,22 @@ const business = {
   reviewCount: 90
 };
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
+const businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hungrii</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses}/>
-    </div>
-  );
+class App extends React.Component {
+
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy }`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hungrii</h1>
+        <SearchBar  searchYelp={this.searchYelp}/>
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
-
 export default App;
